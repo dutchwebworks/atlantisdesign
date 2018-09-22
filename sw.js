7cf5280e7682a8f1466aa8cebd3c973475fab65c
Dennis Burger <dennis@dutchwebworks.nl>
2018-09-22 15:09:45 +0200

    Updated to PWA with proper meta-tags and manifest with icons.

--- sw.js
+++ sw.js
@@ -1,34 +1,61 @@
-self.addEventListener("install", function(event){
-	event.waitUntil(
-		caches.open("assets")
-			.then(function(cache){
-				cache.addAll([
-					"/",
-					"/index.html",
-					"/app.js",
-					"/favicon.ico",
-					"/img/atlantisdesign.svg",
-					"/img/icon-128x128.png",
-					"/img/icon-144x144.png",
-					"/img/icon-152x152.png",
-					"/img/icon-192x192.png",
-					"/img/icon-384x384.png",
-					"/img/icon-512x512.png",
-					"/img/icon-72x72.png",
-					"/img/icon-96x96.png",
-					"https://fonts.googleapis.com/css?family=Roboto:300",
-					"https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmSU5fBBc4AMP6lQ.woff2"
-				])
-			})		
+// SW Version
+const version = "0.1.0";
+
+// Static Cache - App Shell
+const appAssets = [
+	"/img/atlantisdesign.svg",
+	"/favicon.ico",
+	"index.html"
+];
+
+// SW Install
+self.addEventListener("install", e => {
+	e.waitUntill(
+		caches.open(`static-version`)
+			.then(cache => cache.addAll(appAssets))
 	);
-	return self.clients.claim();
 });
 
-self.addEventListener("fetch", function(event){
-	event.respondWith(
-		caches.match(event.request)
-			.then(function(res){
-				return res;
-			})
-	);
+// SW Activate
+self.addEventListener("activate", e => {
+
+	// Clean static cache
+	let cleaned = caches.keys().then(keys => {
+		keys.forEach(key => {
+			if (key !== `static-version` && key.match("static-")) {
+				return caches.delete(key);
+			}
+		});
+	})
+
+	e.waitUntill(cleaned);
+});
+
+// Static cache strategy - Cache with Network Fallback
+const staticCache = (req) => {
+	return caches.match(req).then(cachedRes => {
+
+		// Return cached response if found
+		if (cachedRes) return cachedRes;
+
+		// Fall back to network
+		return fetch(req).then(networkRes => {
+
+			// Update cache with network request
+			caches.open(`static-version`)
+				.then(cache => cache.put(req, networkRes));
+
+			// Return clone of Network Response
+			return networkRes.clone();
+		});
+	})
+}
+
+// SW Fetch
+self.addEventListener("fetch", e => {
+
+	// App shell
+	if (e.request.url.match(location.origin)) {
+		e.respondWith(staticCache(e.request));
+	}
 });
