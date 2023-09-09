42df25ebda9e6b383b0c57ded68229aef66328a8
Dennis Burger <dennis@dutchwebworks.nl>
2023-09-09 16:40:40 +0200

    Refactored to working meta-data like title and description with canonical URL. Added const file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://old.atlantisdesign.nl',
+    compressHTML: false,
+    trailingSlash: 'never',
     markdown: {
         shikiConfig: {
             theme: 'dracula',
