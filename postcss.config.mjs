58b7cc76dc6f864a9db93363b0d94512ab93da1b
Dennis Burger <dennis@dutchwebworks.nl>
2025-08-08 19:56:48 +0200

    Added TailwindCSS config

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+
+import autoprefixer from 'autoprefixer';
+import tailwindcssPostcss from '@tailwindcss/postcss';
+
+export default {
+    plugins: [
+        tailwindcssPostcss({ config: './tailwind.config.mjs' }),
+        autoprefixer,
+    ],
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+    content: [
+        './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+        './public/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+});
