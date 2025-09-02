10fdf847545c4abf75bfac8f63eff4078d03e9fc
Dennis Burger <dennis@dutchwebworks.nl>
2025-09-02 09:28:08 +0200

    Official way to add TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://old.atlantisdesign.nl",
-    compressHTML: false,
-    trailingSlash: "never",
-    markdown: {
-        shikiConfig: {
-            theme: "dracula",
-            wrap: true,
-        },
+  site: "https://old.atlantisdesign.nl",
+  compressHTML: false,
+  trailingSlash: "never",
+
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
